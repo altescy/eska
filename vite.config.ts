c8b7999963f322f65a4221d819af75fd2a2c2fef
altescy <me@altescy.jp>
2025-10-19 07:08:12 +0900

    fix build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,15 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
+import renderer from 'vite-plugin-electron-renderer'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("@chakra-ui/react")) {
-              return "chakra-ui";
-            }
-
-            for (const key of ["react", "lexical"]) {
-              if (id.includes(key)) {
-                return key;
-              }
-            }
-
-            return "vendor";
-          }
-        },
-      },
-    },
+  worker: {
+    format: 'es',
   },
   plugins: [
     react(),
@@ -42,14 +25,8 @@ export default defineConfig({
         // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
         input: path.join(__dirname, 'electron/preload.ts'),
       },
-      // Ployfill the Electron and Node.js API for Renderer process.
-      // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
-      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer: process.env.NODE_ENV === 'test'
-        // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
-        ? undefined
-        : {},
     }),
+    renderer(),
   ],
   resolve: {
     alias: {
